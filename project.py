8dc2e503df4eb003f92cf4e18ee98db9c99d1f0a
b0risR <paterninaboris@gmail.com>
2026-02-17 21:51:42 +0100

    commit from local

---
+++ project.py
@@ -0,0 +1,51 @@
+import os
+import time
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+
+if __name__ == "__main__":
+    conf = SparkConf()
+    conf.setMaster("local[*]")
+    conf.setAppName("conversionInicial")
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+    tiempo_inicio = time.time()
+
+    # esquema para el archivo measurements.txt
+    fileSchema = StructType([
+        StructField("estacion", StringType(), True),
+        StructField("medida", DoubleType(), True)
+    ])
+
+    # Conversion inicial a formato parquet para acelerar el analisis
+    # Solo se efectuara si no existe previamente el directorio
+    path="/measurements"
+    if not os.path.exists(path):
+        DFcrudos = spark.read.csv("measurements.txt",
+                                  sep=";",
+                                  header=False,
+                                  schema=fileSchema)
+        DFcrudos.write.mode("ignore").parquet("measurements", compression="snappy")
+
+    # inicio del analisis con los archivos parquet
+    DFmedidas = spark.read.parquet("measurements")
+    DFmedidas.createOrReplaceTempView("SQLdf")
+    query = '''SELECT estacion, MIN(medida) AS temp_min, 
+                      MAX(medida) AS temp_max, AVG(medida) AS temp_promedio
+               FROM SQLdf
+               GROUP BY estacion
+               ORDER BY estacion'''
+    analisis = spark.sql(query)
+    analisis.write.csv("/opt/spark/work-dir/output", mode="overwrite", header=True)
+
+    tiempo_final = time.time()
+
+    print(f"Tiempo de ejecucion total: {tiempo_final - tiempo_inicio:.2f} segundos")
+    print("revisa el DAG en http://localhost:4040")
+    # pausa por 5 minutos para entrar a
+    # http://localhost:4040
+    # comment out para eliminar el retardo
+    time.sleep(300)
+
+    spark.stop()
